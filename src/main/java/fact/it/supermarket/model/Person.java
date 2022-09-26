a8efc0acc4037e6b40ecf18591d35bc2707b103f
SohaibIbenhajene <r0883629@student.thomasmore.be>
2022-09-26 14:34:28 +0200

    Upload project

---
+++ src/main/java/fact/it/supermarket/model/Person.java
@@ -0,0 +1,35 @@
+//Ibenhajene Sohaib r0883629
+package fact.it.supermarket.model;
+
+public class Person {
+    private String firstName;
+    private String surName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String surName) {
+        this.firstName = firstName;
+        this.surName = surName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String toString(){
+        return this.surName.toUpperCase() + " " + this.firstName;
+    }
+}
